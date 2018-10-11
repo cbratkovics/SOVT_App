bfb7175b5680754dfeb832ce3dae8a1d8d5e48be
Christopher Bratkovics <christopher.bratkovics@uvm.edu>
2018-10-11 10:39:14 -0400

    "changed java classes for main app interfaces to empty activities"

--- app/src/main/java/com/bignerdranch/android/sovt_app/Schedule.java
+++
@@ -1,4 +0,0 @@
-package com.bignerdranch.android.sovt_app;
-
-public class Schedule {
-}
--- app/src/main/java/com/bignerdranch/android/sovt_app/Teams.java
+++ app/src/main/java/com/bignerdranch/android/sovt_app/Teams.java
@@ -3,11 +3,11 @@ package com.bignerdranch.android.sovt_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class General extends AppCompatActivity {
+public class Teams extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_general);
+        setContentView(R.layout.activity_teams);
     }
 }
