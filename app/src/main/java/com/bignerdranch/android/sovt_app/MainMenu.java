9b4d1e1072fcaca03e2b9650c645628e98ab273f
Christopher Bratkovics <christopher.bratkovics@uvm.edu>
2018-10-11 10:31:54 -0400

    "full project with all files are now in the repo"

--- app/src/main/java/com/bignerdranch/android/sovt_app/MainMenu.java
+++ app/src/main/java/com/bignerdranch/android/sovt_app/MainMenu.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainMenu extends AppCompatActivity {
 
+    //this is a class
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
