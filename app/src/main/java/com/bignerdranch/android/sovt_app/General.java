2325443c652fea3d6437ef9e0f7c60f344b0ae8b
PJ <pauljack.solomon@gmail.com>
2018-11-15 11:02:00 -0500

    Merged General Info with Toolbar.

--- app/src/main/java/com/bignerdranch/android/sovt_app/General.java
+++ app/src/main/java/com/bignerdranch/android/sovt_app/General.java
@@ -5,9 +5,13 @@ import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 
 import java.io.File;
 
@@ -27,6 +31,9 @@ public class General extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general);
         Log.d(TAG, "GeneralInfo onCreate(Bundle) called");
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //return to home page button
 
@@ -44,5 +51,31 @@ public class General extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.go_home:
+                Intent intent = new Intent(General.this, MainMenu.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
