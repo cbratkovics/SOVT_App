0c60955bf0d7de53e7decaae1fe65d8c30c8bec1
Xuanyou Liu <xuanyou.liu@uvm.edu>
2018-11-15 23:38:41 +0800

    Changing schedule part

--- app/src/main/java/com/bignerdranch/android/sovt_app/Schedule.java
+++ app/src/main/java/com/bignerdranch/android/sovt_app/Schedule.java
@@ -1,36 +1,47 @@
 package com.bignerdranch.android.sovt_app;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Schedule extends AppCompatActivity {
 
-    private Button mHomeButton;
-    private static final String TAG = "Schedule";
+    private TextView mTextMessage;
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG, "Schedule onSaveInstanceState");
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_general_schedule:
+                    mTextMessage.setText(R.string.detail_general);
+                    mTextMessage.setCompoundDrawables(null, null, null, null);
+                    return true;
+                case R.id.navigation_competition_schedule:
+                    mTextMessage.setText("");
+                    Drawable c_schedule = getResources().getDrawable(R.drawable.competition);
+                    mTextMessage.setCompoundDrawablesWithIntrinsicBounds(null, null, null,c_schedule);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        Log.d(TAG, "Schedule onCreate(Bundle) called");
 
-        //return to home page button
-        mHomeButton = (Button) findViewById(R.id.home_button);
-        mHomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Schedule.this, MainMenu.class));
-            }
-        });
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
