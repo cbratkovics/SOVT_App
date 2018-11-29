40f0b2384bf6c8dcc479af1ab3b8e28da13c5fc5
Nickolas.Castle <ncastle@uvm.edu>
2018-11-29 12:41:39 -0500

    All branches merged - add file to .gitignore that kept creating merge conflicts

--- app/src/main/java/com/bignerdranch/android/sovt_app/Schedule.java
+++ app/src/main/java/com/bignerdranch/android/sovt_app/Schedule.java
@@ -27,7 +27,7 @@ public class Schedule extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
+
         LinearLayout mButtonGeneral = findViewById(R.id.general_schedule_button);
         LinearLayout mButtonCompetition = findViewById(R.id.competition_schedule_button);
         final ImageButton mImageButtonGeneral = findViewById(R.id.general_schedule_img);
